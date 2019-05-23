d61d9103af8f4625189a933d1ba788bfdb932127
HoangHS1604 <gravelysin44@gmail.com>
2019-05-23 19:29:38 +0700

    sd

--- application/views/insertItem.php
+++ application/views/insertItem.php
@@ -8,7 +8,7 @@
               <div class="box-body">
                 <div class="form-group">
                   <label for="exampleInputEmail1">Name</label>
-                  <input type="text" class="form-control" id="txtName" placeholder="Enter Name of prfghfdffghoduct" name="txtName">
+                  <input type="text" class="form-control" id="txtName" placeholder="Enter Name of prfghassfdffghoduct" name="txtName">
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1">Price</label>
